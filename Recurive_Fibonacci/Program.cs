d92750634adbf6b62920f6a53e41391fe095f86e
Ibrahim Ugurlu <ibrahimugurlu18@outlook.com>
2022-03-12 11:16:30 +0300

    Add project files.

---
+++ Recurive_Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Recurive_Fibonacci
+{
+    public static class Recursive
+    {
+
+        public static int Fibo(this int param)
+        {
+            if(param<2)
+            {
+                return param;
+
+            }
+            else
+            {
+                return Fibo(param - 1) + Fibo(param - 2);
+
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+              Console.WriteLine(i.Fibo());
+            }
+            Console.Read();
+        }
+    }
+}
